e98b64472b4830ba1253423579eb28a80104232f
Jiyang <lijiyang727@gmail.com>
2017-07-09 22:00:03 +1000

    Add Team B score counting

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
   int scoreTeamA = 0;
+  int scoreTeamB = 0;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +44,38 @@ public class MainActivity extends AppCompatActivity {
    */
   public void displayForTeamA(int score) {
     TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+    scoreView.setText(String.valueOf(score));
+  }
+
+  /**
+   * Increase the score for Team A by 1 point.
+   */
+  public void addOneForTeamB(View v) {
+    scoreTeamB += 1;
+    displayForTeamB(scoreTeamB);
+  }
+
+  /**
+   * Increase the score for Team A by 2 points.
+   */
+  public void addTwoForTeamB(View v) {
+    scoreTeamB += 2;
+    displayForTeamB(scoreTeamB);
+  }
+
+  /**
+   * Increase the score for Team A by 3 points.
+   */
+  public void addThreeForTeamB(View v) {
+    scoreTeamB += 3;
+    displayForTeamB(scoreTeamB);
+  }
+
+  /**
+   * Displays the given score for Team A.
+   */
+  public void displayForTeamB(int score) {
+    TextView scoreView = (TextView) findViewById(R.id.team_b_score);
     scoreView.setText(String.valueOf(score));
   }
 }
